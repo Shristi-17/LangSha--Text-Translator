5045a94cf5f172367a6a8c9f2965acccce115f6a
Shristi-17 <shristi.chauhan2020@gmail.com>
2025-05-08 14:11:14 +0530

    port updated

--- app.py
+++ app.py
@@ -53,7 +53,8 @@ def index():
     return render_template('index.html', language_columns=show_supported_languages())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
